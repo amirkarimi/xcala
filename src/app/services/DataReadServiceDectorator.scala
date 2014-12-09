1614748179c5d33987a59e7598a9442bb2b987a5
Amir Karimi <a.karimi.k@gmail.com>
2014-12-10 00:58:53 +0330

    Add missing methods to decorator services

--- src/app/services/DataReadServiceDectorator.scala
+++ src/app/services/DataReadServiceDectorator.scala
@@ -4,7 +4,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import reactivemongo.bson._
 import reactivemongo.core.commands.LastError
-import xcala.play.models.{ QueryOptions, DataWithTotalCount }
+import xcala.play.models.{ QueryOptions, SortOptions, DataWithTotalCount }
 import reactivemongo.api.collections.GenericQueryBuilder
 
 trait DataReadServiceDecorator[A, B] extends DataReadService[B] {
@@ -17,18 +17,20 @@ trait DataReadServiceDecorator[A, B] extends DataReadService[B] {
   
   def findQuery(query: BSONDocument): GenericQueryBuilder[BSONDocument, BSONDocumentReader, BSONDocumentWriter] = service.findQuery(query)
 
-  def findById(id: BSONObjectID): Future[Option[B]] = service.findById(id).map(opt => opt.map(mapModel))
+  def findById(id: BSONObjectID): Future[Option[B]] = service.findById(id).map(_.map(mapModel))
   
-  def find(query: BSONDocument): Future[List[B]] = service.find(query).map(list => list.map(mapModel))
+  def find(query: BSONDocument): Future[List[B]] = service.find(query).map(_.map(mapModel))
   
-  def find(query: BSONDocument, sort: BSONDocument): Future[List[B]] = service.find(query, sort).map(list => list.map(mapModel))
+  def find(query: BSONDocument, sort: BSONDocument): Future[List[B]] = service.find(query, sort).map(_.map(mapModel))
   
-  def findOne(query: BSONDocument): Future[Option[B]] = service.findOne(query).map(opt => opt.map(mapModel))
+  def findOne(query: BSONDocument): Future[Option[B]] = service.findOne(query).map(_.map(mapModel))
   
   def count(query: BSONDocument): Future[Int] = service.count(query)
   
   def find(query: BSONDocument, queryOptions: QueryOptions): Future[DataWithTotalCount[B]] = 
     service.find(query, queryOptions).map(dataWithTotalCount => dataWithTotalCount.copy(data = dataWithTotalCount.data.map(mapModel)))
+    
+  def find(query: BSONDocument, sortOptions: SortOptions): Future[Seq[B]] = service.find(query, sortOptions).map(_.map(mapModel))
   
   def getIdFromModel(model: B): Option[BSONObjectID]
 }
