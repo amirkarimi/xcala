5beaa8d87d9c5c03296cc6ce9b782fb99bdee097
Amir Karimi <a.karimi.k@gmail.com>
2015-04-10 18:57:13 +0430

    Add other useful methods for multilang text helper

--- src/app/xcala.play/extensions/MultilangTextHelper.scala
+++ src/app/xcala.play/extensions/MultilangTextHelper.scala
@@ -5,8 +5,25 @@ import play.api.i18n.Lang
 
 object MultilangTextHelper {
   implicit class MultilangTextViewer(val value: List[MultilangText]) extends AnyVal {
+    /**
+     * Returns the value of specified language if available, otherwise returns empty string.
+     */
     def getLangValue(implicit lang: Lang) = {
-      value.filter(_.lang == lang.code).map(_.value).headOption.getOrElse("")
+      getLangValueOpt.getOrElse("")
+    }
+    
+    /**
+     * Returns the value of specified language if available, otherwise returns the first existing language.
+     */
+    def getLangValueOrExisting(implicit lang: Lang) = {
+      getLangValueOpt.getOrElse(value.map(_.value).headOption.getOrElse(""))
+    }
+    
+    /**
+     * Returns the value of specified language if available.
+     */
+    def getLangValueOpt(implicit lang: Lang) = {
+      value.filter(_.lang == lang.code).map(_.value).headOption
     }
   }
 }
