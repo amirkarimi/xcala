716121fe2bbd04726f0f70e7bd06097d29eacf4a
Soheil Jafari <jafari.sst@gmail.com>
2016-11-13 14:19:23 +0330

    Fix pertian date string

--- src/app/xcala.play/extensions/PersianUtils.scala
+++ src/app/xcala.play/extensions/PersianUtils.scala
@@ -12,7 +12,7 @@ object PersianUtils {
 	    if (lang.code == "fa") {
 	      val gDate = SimpleDate(dateTime.year.get, dateTime.monthOfYear.get, dateTime.dayOfMonth.get)
 	      val pDate = gregorianToPersian(gDate)
-	      s"${pDate.year}/${pDate.month}/${pDate.day}" + 
+	      s"${pDate.year}/${"%02d".format(pDate.month)}/${"%02d".format(pDate.day)}" + 
       		(if (addTime) { " " + dateTime.toString("HH:mm") } else { "" })
 	    } else {
 	      dateTime.toString("yyyy-MM-dd" + (if (addTime) { " HH:mm" } else { "" }))
