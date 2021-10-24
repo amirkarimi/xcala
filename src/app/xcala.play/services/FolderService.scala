310c367d3475c6bc345cd173699ff2284c3149cc
Soheil Jafari <jafari.sst@gmail.com>
2021-10-25 02:26:11 +0330

    Comment unused service

--- src/app/xcala.play/services/FolderService.scala
+++ src/app/xcala.play/services/FolderService.scala
@@ -1,12 +1,13 @@
-package xcala.play.services
-
-import play.api.Configuration
-
-import scala.concurrent.ExecutionContext
-import xcala.play.models.Folder
-import reactivemongo.api.bson._
-
-class FolderService(implicit val ec: ExecutionContext, val configuration: Configuration) extends DataCrudService[Folder] {
-  val collectionName = "folders"
-  val documentHandler = Macros.handler[Folder]  
-}
+//TODO :Fix Soheil
+//package xcala.play.services
+//
+//import play.api.Configuration
+//
+//import scala.concurrent.ExecutionContext
+//import xcala.play.models.Folder
+//import reactivemongo.api.bson._
+//
+//class FolderService(implicit val ec: ExecutionContext, val configuration: Configuration) extends DataCrudService[Folder] {
+//  val collectionName = "folders"
+//  val documentHandler = Macros.handler[Folder]
+//}
