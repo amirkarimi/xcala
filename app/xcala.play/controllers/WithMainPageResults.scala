f2e09bbf05e95ebab558b04b13fb6fff9f61eafa
Soheil Jafari <jafari.sst@gmail.com>
2021-11-08 01:38:49 +0330

    update-with-main-page-result

--- app/xcala.play/controllers/WithMainPageResults.scala
+++ app/xcala.play/controllers/WithMainPageResults.scala
@@ -5,13 +5,13 @@ import play.api.i18n.Messages
 
 trait WithMainPageResults extends Results {
 
-  def mainPageRoute: Call
+  def mainPageRoute(implicit requestHeader: RequestHeader): Call
 
-  protected def successfulResult(message: String)(implicit messages: Messages): Result = {
+  protected def successfulResult(message: String)(implicit messages: Messages, requestHeader: RequestHeader): Result = {
     Redirect(mainPageRoute).flashing("success" -> message)
   }
 
-  protected def failedResult(message: String)(implicit messages: Messages): Result = {
+  protected def failedResult(message: String)(implicit messages: Messages, requestHeader: RequestHeader): Result = {
     Redirect(mainPageRoute).flashing("error" -> message)
   }
 }
