f9352f8327ecdbaeb4e1fc1fe86507d39ac32d58
keivanabdi <keivan.a.khorsand@gmail.com>
2022-08-13 09:26:36 +0000

    Add ReturnUrlValidationHelper

---
+++ app/xcala.play/utils/ReturnUrlValidationHelper.scala
@@ -0,0 +1,19 @@
+package xcala.play.utils
+
+import java.net.URI
+
+object ReturnUrlValidationHelper {
+
+  private def isRelativeUrl(url: String): Boolean = {
+    val uri = new URI(url)
+    !uri.isAbsolute
+  }
+
+  def withReturnUrlValidation[A](maybeReturnUrl: Option[String])(validatedReturnUrlToResult: Option[String] => A): A = {
+    val validatedReturnUrl = maybeReturnUrl.filter { returnUrl =>
+      isRelativeUrl(returnUrl) && !returnUrl.toLowerCase.contains("javascript")
+    }
+    validatedReturnUrlToResult(validatedReturnUrl)
+  }
+
+}
