6453487588282be3de74ec72c4dc1324eb04f6d5
keivanabdi <keivan.a.khorsand@gmail.com>
2024-02-19 08:19:03 +0000

    url filter fix

--- app/xcala.play/extensions/UrlParamsFilter.scala
+++ app/xcala.play/extensions/UrlParamsFilter.scala
@@ -7,19 +7,18 @@ import play.api.mvc.Result
 
 import javax.inject.Inject
 import scala.concurrent.Future
-import scala.util.matching.Regex
 
 object UrlParamsFilter {
-  val harmfulPattern: Regex = ".*[\\(\\)]+.*".r
-  def isUrlParamSafe(urlParam: String): Boolean = harmfulPattern.findFirstMatchIn(urlParam).isEmpty
+  val potentiallyHarmfulPattern: String = "[\\(\\)]+"
 }
 
 class UrlParamsFilter @Inject() (implicit val mat: Materializer) extends Filter {
 
   override def apply(nextFilter: RequestHeader => Future[Result])(requestHeader: RequestHeader)
       : Future[Result] = {
-    val filteredQueryMaps = requestHeader.target.queryMap.filter { case (_, values) =>
-      values.forall(UrlParamsFilter.isUrlParamSafe)
+    val filteredQueryMaps = requestHeader.target.queryMap.map { case (key, values) =>
+      key ->
+        values.map(_.replaceAll(UrlParamsFilter.potentiallyHarmfulPattern, " ").trim.replaceAll(" +", " "))
     }
 
     val filteredTarget = requestHeader.target.withQueryString(filteredQueryMaps)
--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := """xcala.play"""
 
 organization := "com.xcala"
 
-version := "1.0.16"
+version := "1.0.18"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
